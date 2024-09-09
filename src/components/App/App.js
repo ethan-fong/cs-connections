d7590e696088aedd9211c1d8196189a5bd404cfa
unknown <ethan.fong@mail.utoronto.ca>
2024-09-08 23:00:38 -0700

    fix hash router again

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Header from "../Header";
 import Game from "../Game";
 
